300575ed306e5660a4ccb03d70a4b8a8a1f9dd9a
VeronicaLombardi <veronica.lombardi98@gmail.com>
2025-03-31 13:54:16 +0200

    Step5

---
+++ Step5/0.libraries.R
@@ -0,0 +1,21 @@
+# List of required packages
+packages <- c(
+  "PatientProfiler", "tidyverse", "readxl", "ggeasy", "ggpubr", "fgsea", "ggplot2",
+  "cowplot", "writexl", "igraph", "stringr", "httr", "plyr", "pheatmap", 
+  "dendextend", "RColorBrewer", "broom", "GGally", "devtools", "ggfortify", 
+  "ggrepel", "TCGAbiolinks", "dplyr", "readr", "survival", "survminer", 
+  "gridExtra", "org.Hs.eg.db", "fs"
+)
+
+# Function to check and install missing packages
+install_if_missing <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+  }
+}
+
+# Apply the function to all packages
+lapply(packages, install_if_missing)
+
+# Load all packages
+lapply(packages, library, character.only = TRUE)
